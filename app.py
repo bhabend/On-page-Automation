bde3d501ce469411e1cf6394d99cdf596d6f1797
bhabend <bhabendeka@gmail.com>
2025-07-31 20:41:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,11 +17,13 @@ if st.button("Run Audit"):
         st.warning("Please enter at least one URL.")
     else:
         # Setup Headless Chrome for Render
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options = Options()
+chrome_options.binary_location = "/usr/bin/google-chrome"
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
 
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
