852cc306e65a94a8f56e21086ea9dd13c2a4be7b
bhabend <bhabendeka@gmail.com>
2025-08-01 16:35:29 +0530

    Update serp_api.py

--- serp_api.py
+++ serp_api.py
@@ -1,24 +1,27 @@
 import os
 import requests
 
-SERP_API_KEY = os.getenv('SERPHOUSE_API_KEY')
+SERP_API_KEY = os.getenv('SERPAPI_KEY')
 
 def get_serp_data(keyword, location='us', language='en'):
     if not SERP_API_KEY:
-        raise ValueError("SERPHOUSE_API_KEY is not set in environment variables.")
+        raise ValueError("SERPAPI_KEY is not set in environment variables.")
 
-    url = "https://api.serphouse.com/serp/live"
-    payload = {
-        "api_key": SERP_API_KEY,
+    params = {
+        "engine": "google",
         "q": keyword,
-        "gl": location,
+        "location": location,
         "hl": language,
-        "num": 10
+        "num": "10",
+        "api_key": SERP_API_KEY
     }
-    response = requests.post(url, json=payload)
+
+    url = "https://serpapi.com/search.json"
+    response = requests.get(url, params=params)
     results = response.json()
 
-    if 'organic' in results.get('data', {}):
-        return [{"position": item['position'], "title": item['title'], "url": item['url']} for item in results['data']['organic']]
+    if 'organic_results' in results:
+        return [{"position": item['position'], "title": item['title'], "url": item['link']} for item in results['organic_results']]
     else:
+        print("SerpAPI Response:", results)  # Debugging line to see API response in Render Logs
         return []
